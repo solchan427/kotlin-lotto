8f7bd4925728ca6cd39b0a3f73b1e77ac20b1c1a
solchan427 <qkrthfcks@gmail.com>
2024-09-10 10:59:18 +0900

    feat: 로또 당첨 번호와 보너스 번호는 중복될 수 없다 구현 및 테스트 작성

--- src/main/kotlin/WinningLotto.kt
+++ src/main/kotlin/WinningLotto.kt
@@ -1,5 +1,11 @@
 class WinningLotto(val numbers: Set<LottoNumber>, val bonus: LottoNumber) {
 
+    init {
+        if (numbers.contains(bonus)) {
+            throw IllegalArgumentException("Lotto $bonus has duplicated number")
+        }
+    }
+
     fun match(lotto: Lotto): Rank {
         val matchCount: Int = numbers.intersect(lotto.numbers).count()
         val matchBonus: Boolean = lotto.numbers.contains(bonus)
--- src/test/kotlin/WinningLottoTest.kt
+++ src/test/kotlin/WinningLottoTest.kt
@@ -1,5 +1,6 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class WinningLottoTest {
 
@@ -28,5 +29,22 @@ class WinningLottoTest {
         )
         val rank = winningLotto.match(lotto)
         assertThat(rank).isEqualTo(Rank.FIRST)
+    }
+
+    @Test
+    fun `로또 당첨 번호와 보너스 번호는 중복될 수 없다`() {
+        assertThrows<IllegalArgumentException> {
+            WinningLotto(
+                setOf(
+                    LottoNumber(1),
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                    LottoNumber(6)
+                ),
+                LottoNumber(1)
+            )
+        }
     }
 }
